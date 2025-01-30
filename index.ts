13f22d43cb65fde923ecd1d3cae2d236c57bbba2
Ashok Lama <ashok.lama@innovatetech.co>
2025-01-30 19:01:27 +0545

    [emal]

--- index.ts
+++ index.ts
@@ -2,12 +2,14 @@ import express from "express";
 import { env } from "./config";
 import { mainRouter } from "./routes";
 import { Database } from "./db";
+import cors from "cors";
 
 const app = express();
 const databse = new Database();
 
 databse.connect();
 
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use("/api", mainRouter);
 
